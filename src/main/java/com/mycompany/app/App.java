f1575498444216f685cc25793c23dbacbcb98db8
Anas <anasgaamal@gmail.com>
2025-03-12 14:55:39 +0000

    add while loop example

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -2,11 +2,25 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
-
 package com.mycompany.app;
+
+import java.util.*;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+
+        int numOfRuns = 5;
+
+        int number = 0;
+
+        while(number < numOfRuns) { // run while loop as long as array length less than numOfGrades
+            number = number + 1;
+            System.out.println("Number value is: " + number);
+            // if (number == 3) {
+            //     break;
+            // }
+        }
+
+        System.out.println("Done");
     }
 }
